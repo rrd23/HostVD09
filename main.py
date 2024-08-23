4df0352730a8f194716b51a37aed06c0393c7f4d
rrd23 <123895790+rrd23@users.noreply.github.com>
2024-08-23 18:30:43 +0300

    final

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import app, db
 from app.models import User
-
-with app.app_context():  # После первого запуска эту строку можно удалить
+# После первого запуска эту строку можно удалить
+with app.app_context():
     db.create_all()  # После первого запуска эту строку можно удалить
 
 if __name__ == '__main__':
